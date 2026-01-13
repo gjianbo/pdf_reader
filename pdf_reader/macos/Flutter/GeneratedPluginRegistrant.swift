9c0d7e5cce2b0d97ea2abb6f909faa0b87cd74e4
jianboguo <gjianbo@qq.com>
2026-01-13 16:20:24 +0800

    修复bug

--- pdf_reader/macos/Flutter/GeneratedPluginRegistrant.swift
+++ pdf_reader/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,6 +5,7 @@
 import FlutterMacOS
 import Foundation
 
+import audio_service
 import audio_session
 import audioplayers_darwin
 import device_info_plus
@@ -23,6 +24,7 @@ import wakelock_plus
 import webview_flutter_wkwebview
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  AudioServicePlugin.register(with: registry.registrar(forPlugin: "AudioServicePlugin"))
   AudioSessionPlugin.register(with: registry.registrar(forPlugin: "AudioSessionPlugin"))
   AudioplayersDarwinPlugin.register(with: registry.registrar(forPlugin: "AudioplayersDarwinPlugin"))
   DeviceInfoPlusMacosPlugin.register(with: registry.registrar(forPlugin: "DeviceInfoPlusMacosPlugin"))
